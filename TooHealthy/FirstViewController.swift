c49731786c64a14597464b1e47fb9581cdb0c931
Artem Belkov <bestk1ngarthur@aol.com>
2019-11-15 20:07:02 +0200

    Initial Commit

---
+++ TooHealthy/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TooHealthy
+//
+//  Created by Artem Belkov on 15.11.2019.
+//  Copyright © 2019 Artem Belkov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TooHealthy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TooHealthy
+//
+//  Created by Artem Belkov on 15.11.2019.
+//  Copyright © 2019 Artem Belkov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
